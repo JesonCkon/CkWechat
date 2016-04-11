6d883729e27ac7bf4adec30688420d2ecd7feb1c
Jeson.ckon <ckontem@gmail.com>
2016-04-11 11:40:29 +0800

    upgrade add interface models

--- src/Service/DataStatService.php
+++ src/Service/DataStatService.php
@@ -28,6 +28,6 @@ class DataStatService implements ServiceInterface
         };
         $obj->dataStatIface = function ($obj) {
           return new DataStatIface($obj);
-        };        
+        };
     }
 }
